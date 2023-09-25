7909d786063d58199b0597b5809127e256769af3
CHRISTIAN ABISHAI <https://github.com/CHRISTIANABISHAI>
2023-09-25 21:08:35 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+// import LoginPage from './Loginpage.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+
+    {/* <LoginPage/> */}
+  </React.StrictMode>
+)
